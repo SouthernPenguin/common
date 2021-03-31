758bfebf0eb7060ae2d9b3169b8e46cb87dbc5f0
SouthernPenguin <940583551@qq.com>
2021-03-31 15:48:55 +0800

    navigateTo

--- wxCommon/wxCommonAPI.js
+++ wxCommon/wxCommonAPI.js
@@ -2,26 +2,22 @@
  * 微信常见AIP集合
  */
 const wxCommonAPI = {
-  /**
-   * 分享朋友圈
-   */
   showShareMenu() {
     wx.showShareMenu({
       withShareTicket: true,
-      menus: ['shareAppMessage', 'shareTimeline'],
-    });
+      menus: ['shareAppMessage', 'shareTimeline']
+    })
   },
-  /**
-   * 设置标题
-   */
   setNavigationBarTitle(title) {
     wx.setNavigationBarTitle({
-      title,
-    });
+      title
+    })
   },
-  /**
-   * 弹窗
-   */
+  phoneNumber(phoneNumber) {
+    wx.makePhoneCall({
+      phoneNumber
+    })
+  },
   showToast(title, icon = 'none', image = '', mask = false) {
     wx.showToast({
       title,
@@ -31,17 +27,11 @@ const wxCommonAPI = {
       success: (res) => {},
       fail: (res) => {},
       complete: (res) => {},
-    });
+    })
   },
-  /**
-   * 弹窗隐藏
-  */
   hideToast() {
-    wx.hideToast();
+    wx.hideToast()
   },
-  /**
-   * 微信支付
-  */
   requestPayment(timeStamp, nonceStr, packageSub, signType, paySign) {
     return new Promise((resolve, reject) => {
       wx.requestPayment({
@@ -50,15 +40,20 @@ const wxCommonAPI = {
         package: packageSub,
         signType,
         paySign,
-        success: (res) => {
-          resolve(res);
+        success: (res) => { 
+          resolve(res)
         },
-        fail(err) {
-          reject(err);
-        },
-      });
-    });
+        fail(err) { 
+          reject(err)
+        }
+      })
+    })
   },
-};
+  navigateTo(url){
+    wx.navigateTo({
+      url
+    })
+  }
+}
 
-export default wxCommonAPI;
+export default wxCommonAPI
